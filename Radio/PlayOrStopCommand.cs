cdd68bf2b6a0a75405b0e18f69d038b750f50547
roli <roli@cock.li>
2016-05-16 01:54:18 -0500

    Adding shortcut to play/stop.
    Added a command that allows us to play or stop the music by pressing the
    space key, or the Play/Pause media key. Deleted the accelerator keys
    that did this job before.

---
+++ Radio/PlayOrStopCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Radio
+{
+    class PlayOrStopCommand
+    {
+        static RoutedUICommand _playOrStop;
+
+        public static RoutedUICommand PlayOrStop
+        {
+            get { return _playOrStop; }
+        }
+
+        static PlayOrStopCommand()
+        {
+            InputGestureCollection gesture = new InputGestureCollection();
+            gesture.Add(new KeyGesture(Key.Space, ModifierKeys.None, "Space"));
+            gesture.Add(new KeyGesture(Key.MediaPlayPause, ModifierKeys.None, "PlayPause"));
+
+            _playOrStop = new RoutedUICommand("PlayOrStop", "PlayOrStop", typeof(PlayOrStopCommand), gesture);
+        }
+    }
+}
