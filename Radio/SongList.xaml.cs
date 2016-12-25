3f633536d69482ad9c28dd6708849069decaf131
roli <roli@cock.li>
2016-12-24 23:57:15 -0600

    Async for favorite list.

--- Radio/SongList.xaml.cs
+++ Radio/SongList.xaml.cs
@@ -34,14 +34,19 @@ namespace Radio
             InitializeComponent();
             //newCont.ItemsSource = items;
             //newCont.ItemsSource = Database.GetRangeOfRecords(Page.CurrentNumber, Updater.DBConnection).Result;
+
+            // Going to make CreatePagination public to execute in a task
             CreatePagination();
+
+            //CreatePaginationAsync();
+
             /*favCont.ItemsSource = items;
             delCont.ItemsSource = items;*/
             /*SongContainer.DataContext = items;
             SongContainer.ItemsSource = items;*/
         }
 
-        async void CreatePagination()
+        public async void CreatePagination()
         {
             /*int numberOfPages = await Database.NumberOfPages(Updater.DBConnection);
             PaginationNumber.Text = numberOfPages.ToString();*/
@@ -56,6 +61,24 @@ namespace Radio
             PaginationNumber.ItemsSource = items;*/
             PaginationNumber.ItemsSource = Page.GetNewButtonList(currPageList);
 
+        }
+
+        /*public async void CreatePaginationAsync()
+        {
+            Task t = Task.Run(() =>
+            {
+                PaginationNumber.ItemsSource = Page.GetNewButtonList(currPageList);
+            });
+
+            await t;
+        }*/
+
+        public async void CreatePaginationAsync()
+        {
+            //PaginationNumber.ItemsSource = Page.GetNewButtonList(currPageList);
+            Page._controlList = currPageList;
+            PaginationNumber.ItemsSource = Page.GetNewButtonList();
+            currPageList.ItemsSource = SongFromList.CurrentList;
         }
 
     }
